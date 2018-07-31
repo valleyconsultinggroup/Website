b1932e1378caaf1f4c7bb9d497c696b12bb353ff
John Boda Yang <byjohnyang@gmail.com>
2018-07-30 22:39:26 -0700

    Setting up Flask backend + introducing basic html pages

---
+++ server.py
@@ -0,0 +1,30 @@
+import sys, os
+
+# Imports
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+from flaskext.markdown import Markdown
+
+# Configuration
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
+markdown_manager = Markdown(app, extensions=['fenced_code'], output_format='html5',)
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
